e084accbc3fe1db4068eb7083f2b7ae71c433dee
Raul Cortinas <114097137+Rcurt17@users.noreply.github.com>
2023-09-15 13:03:33 -0400

    Add files via upload

--- Student_and_Quiz.java
+++ Student_and_Quiz.java
@@ -6,41 +6,53 @@ class Student_and_Quiz {
         
         Student name = new Student();
         name.getName();
-        
+        Student quiz = new Student();
+        quiz.addQuiz();
+
 
         }
     }
     class Student {
 
+
             public String getName() {
                 Scanner input = new Scanner(System.in);
+
                 System.out.print("Enter your name: ");
+                
                 String name = input.nextLine();
-                System.out.println(name);
+                
                 input.close();
+                
                 return name;
 
             }
 
-            public int addQuiz(int score) {
+            public int addQuiz() {
                 int numberOfQuizzez = 0;
+
+                int total = 0;
+                
                 Scanner input = new Scanner(System.in);
 
                 while (numberOfQuizzez < 5) {
                     System.out.print("Enter quiz scores:  ");
+
                     int quizScore = input.nextInt();
+                    
+                    int totalQuizScore = total + quizScore;
+                    
                     numberOfQuizzez++;
 
                     if (quizScore == 5) {
                         
-
+                        System.out.println("Your total quiz score is: " + totalQuizScore);
+                        
+                        input.close();
+                        
+                        break;     
                     }
-
-
                 }
-
-
-                return addQuiz(score);
-
+                return total;
             }
 }

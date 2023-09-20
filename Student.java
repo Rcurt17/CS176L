ac8d5873237911ec5165852d26329334cb16910b
Raul Cortinas <114097137+Rcurt17@users.noreply.github.com>
2023-09-20 11:47:05 -0400

    class Student with 5 quizzez (packaged)

---
+++ Student.java
@@ -0,0 +1,42 @@
+package week2HW;
+
+public class Student {
+	private String name;
+	private double scores;
+	private int testNumber;
+	
+	public Student(String studentName) {
+		name = studentName;
+		scores = 0;
+		testNumber = 0;
+		
+	}
+	
+public String getName() {
+	return name;
+	
+}
+
+public void addQuiz(int score) {
+	scores += score;
+	testNumber += 1;
+	
+}
+
+public double getTotalScore() {
+	return scores;
+	
+}
+
+public double getAverageScore() {
+	return scores / testNumber;
+	
+}
+
+public double getScores() {
+	return scores;
+}
+
+	
+
+}

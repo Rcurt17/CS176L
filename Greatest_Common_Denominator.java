a461778b8806c1ac8a6b054f56f7e48657d532e8
Raul Cortinas <114097137+Rcurt17@users.noreply.github.com>
2023-10-18 20:29:47 -0400

    Week 7 Answers

--- Greatest_Common_Denominator.java
+++ Greatest_Common_Denominator.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 
 public class Greatest_Common_Denominator {
 	
-	final int LOWERLIMIT = 0;
-	final int UPPEERLIMIT = 1000;
-	
 	
 	static int returningGCD(int NUM1, int NUM2) {
 		int greatestCommonDenominator = 1;
 		
 		for (int x = 1; x <= NUM1 && x <= NUM2; x++) {
+			
 			if (NUM1 % x == 0 && NUM2 % x ==0) {
+				
 				greatestCommonDenominator = x;
 			}
 		}
@@ -21,11 +20,34 @@ public class Greatest_Common_Denominator {
 
 	public static void main(String[] args) {
 		
+		final int LOWERLIMIT = 0;
+		final int UPPERLIMIT = 1000;
+		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Input two numbers to find out the GCD: ");
-		int NUM1 = input.nextInt();
-		int NUM2 = input.nextInt();
+		int NUM1 = 0;
+		int NUM2 = 0;
+		
+		boolean goodNumber = false;
+		
+		while (goodNumber == false) {
+			
+			System.out.print("Input two numbers to find out the GCD: ");
+			
+			 NUM1 = input.nextInt();
+			 NUM2 = input.nextInt();
+			
+			if (NUM1 < LOWERLIMIT 
+					|| NUM2 < LOWERLIMIT 
+					|| NUM1 > UPPERLIMIT 
+					|| NUM2 > UPPERLIMIT) {
+				
+				System.out.println("Please enter valid numbers");
+			
+			} else {
+				goodNumber = true;
+			}
+		}
 		
 		int GCD = returningGCD(NUM1, NUM2);
 		
